aa48ac6f52e6ed1914c35a6e3edd9b89d6dafcd2
Muthusrihemadharshini <samuthu2105@gmail.com>
2026-01-11 13:57:20 +0530

    feat: implement Pet class logic with hunger and happiness methods

---
+++ src/PetLogic.js
@@ -0,0 +1,30 @@
+export class Pet {
+    constructor(name){
+        this.name=name;
+        this.hunger=50;
+        this.happiness=50;
+
+}
+
+
+feed(){
+this.hunger= Math.max(0,this.hunger-15);
+this.happiness = Math.min(100,this.happiness+5);
+return `${this.name} is full and happy!`;
+}
+
+
+
+play(){
+this.happiness = Math.min(100,this.happiness+20);
+this.hunger= Math.max(0,this.hunger+10);
+return `${this.name} loved playing with you!`;
+
+}
+
+getStatus(){
+if(this.hunger > 80)return "Starving";
+if(this.happiness <20) return "Very Sad";
+return "Doing Great";
+}
+}
